ce89c7f921b6e77c6fd390476be6c0533a5967ea
praise obah <obahsomto@gmail.com>
2023-04-24 12:15:05 -0700

    second bubble sort commit

--- sort.h
+++ sort.h
@@ -14,7 +14,7 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-} listint_t
+} listint_t;
 
 /* print functions */
 void print_list(const listint_t *list);
